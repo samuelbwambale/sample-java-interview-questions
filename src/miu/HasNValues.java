70057e68cef13aa2738e0ac233bcc3726c30bac3
samuelbwambale <simplesam63@gmail.com>
2020-04-23 00:06:59 -0500

    add FibonacciSeries and HasNValues

---
+++ src/miu/HasNValues.java
@@ -0,0 +1,45 @@
+package miu;
+
+
+
+/*
+ * Write a function named hasNValues which takes an array and an integer n as arguments.
+ * It returns true if all the elements of the array are one of n different values.
+ *
+ * {1, 2, 2, 1}	n=2	returns 1 (because there are 2 different element values, 1 and 2)
+	{1, 1, 1, 8, 1, 1, 1, 3, 3}	n=3	returns 1 (because there are 3 different element values, 1, 3, 8)
+	{1, 2, 3, 4, 5, 6, 7, 8 ,9, 10}	n=10	returns 1 (because there are 10 different element values)
+	{1, 2, 2, 1} n=3	returns 0 (because there are 2 different element values, not 3 as required)
+	{1, 1, 1, 8, 1, 1, 1, 3, 3}	n=2	returns 0 (because there are 3 different element values, not 2 as required)
+	{1, 2, 3, 4, 5, 6, 7, 8 ,9, 10}	n=20	returns 0 (because there are 10 different element values, not 20 as required)
+
+*/
+import java.util.HashMap;
+
+public class HasNValues {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 1, 8, 1, 1, 1, 3, 3};
+        int result = hasNValues(arr, 3);
+        System.out.println(result);
+    }
+
+    public static int hasNValues(int[] a, int n) {
+        HashMap<Integer, Integer> distinct = new HashMap<Integer, Integer>();
+
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (!distinct.containsKey(a[i])) {
+                distinct.put(a[i], a[i]);
+            }
+        }
+        if (distinct.size() == n) {
+            result = 1;
+        }
+        else result = 0;
+
+        return result;
+
+    }
+}
+
---
+++ src/miu/PairedN.java
@@ -0,0 +1,61 @@
+package miu;
+
+import java.util.HashMap;
+
+/*An array is defined to be paired-N if it contains two distinct elements that sum to N for 
+* some specified value of N and the indexes of those elements also sum to N. 
+* Write a function named isPairedN that returns 1 if its array parameter is a paired-N array. 
+* The value of N is passed as the second parameter.
+* 
+* {1, 4, 1, 4, 5, 6}	n=5	returns 1	because a[2] + a[3] == 5 and 2+3==5. In other words, the sum of the values is equal to the sum of the corresponding indexes and both are equal to n (5 in this case).
+	{1, 4, 1, 4, 5, 6}	n=6	returns 1	because a[2] + a[4] == 6 and 2+4==6
+	{0, 1, 2, 3, 4, 5, 6, 7, 8}	n=6	returns 1	because a[1]+a[5]==6 and 1+5==6
+	{8, -8, 8, 8, 7, 7, -7}	-15	0	because although a[1]+a[6]==-15, 1+6!=-15
+	{3}	n=3	returns 0	because the array has only one element
+	{}	n=0	returns 0	because the array has no elements
+*/
+
+public class PairedN {
+	public static void main(String[] args) {
+       
+       int[] arr = {1, 0, 2, 3};
+//       int result = isPairedN(arr, 5);
+       int result = isPairedN_Improved(arr, 5);
+       System.out.println(result);
+   }
+	
+	public static int isPairedN(int[] a, int n) {
+		if(a==null || a.length < 2) {
+			return 0;
+			}
+
+       for (int i = 0; i < a.length; i++) {
+           for (int j = i + 1; j < a.length; j++) {
+               if (a[i] + a[j] == n && i + j == n) {
+                   return 1;
+               }
+           }
+       }
+       return 0;
+   }
+	
+	public static int isPairedN_Improved(int[] a, int n) {
+		if(a == null || a.length < 2) {
+            return 0;
+        }
+        final Integer ONE = new Integer(1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < a.length; i++) {
+            map.put(n - a[i], ONE);
+
+        }
+        for (int j = 0 ; j < a.length; j++) {
+            if (map.containsKey(a[j]) && a[j] != (n - a[j])) {
+                return 1;
+            }
+        }
+        return 0;
+   }
+
+}
+
