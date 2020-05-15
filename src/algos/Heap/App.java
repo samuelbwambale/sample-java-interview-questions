608ae513af16a4a5f5d3de7fcacdec01ebf5caaa
samuelbwambale <simplesam63@gmail.com>
2020-05-15 15:13:24 -0500

    add heap

---
+++ src/algos/Heap/App.java
@@ -0,0 +1,27 @@
+package algos.Heap;
+
+
+public class App {
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        heap.insertItem(5);//heap.printAsDiagram();
+        heap.insertItem(9);//heap.printAsDiagram();
+        heap.insertItem(12);//heap.printAsDiagram();
+        heap.insertItem(20);//heap.printAsDiagram();
+        heap.insertItem(10);//heap.printAsDiagram();
+        heap.insertItem(14);//heap.printAsDiagram();
+        heap.insertItem(18);//heap.printAsDiagram();
+        heap.insertItem(29);//heap.printAsDiagram();
+        heap.insertItem(30);//heap.printAsDiagram();
+        heap.insertItem(13);//heap.printAsDiagram();
+
+        System.out.println("going to remove mean ");
+        heap.removeMin();
+        System.out.println("mean removed");
+
+//        heap.printAsDiagram();
+       heap.deleteItem(14);
+        heap.printAsDiagram();
+
+    }
+}
---
+++ src/algos/Heap/Heap.java
@@ -0,0 +1,319 @@
+package algos.Heap;
+
+
+import java.util.*;
+
+/**
+ * Code shell for a MinHeap that support a deleteItem operation
+ */
+public class Heap {
+    public Node root;
+    Comparable key1 = null;
+    Comparable key2 = null;
+    private Hashtable<Object, Node> locator = new Hashtable<Object, Node>();
+    private Node lastItem;
+    private Node parentOfNextOpenPosition;
+    private Queue<Node> queue = new LinkedList<Node>();
+    private Stack<Node> stack = new Stack<Node>();
+
+    public void insertItem(Object key) {
+        //create new Node n, insert key, and place as child
+        //of parentOfNextOpenPosition
+        //then perform upHeap(n)
+
+        if (root == null) {
+            root = new Node(key, null, null, null);
+            locator.put(key, root);
+            lastItem = root;
+            parentOfNextOpenPosition = root;
+            stack.push(root);
+        } else {
+            innerInsertItem(parentOfNextOpenPosition, key);
+        }
+    }
+
+    private void innerInsertItem(Node parent, Object key) {
+        Node newNode = new Node(key, null, null, parent);
+        if (parent.left == null) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+            Node freshNode = null;
+            while (freshNode == null) {
+                freshNode = queue.poll();
+            }
+            parentOfNextOpenPosition = freshNode;
+        }
+        locator.put(key, newNode);
+        lastItem = newNode;
+        queue.add(newNode);
+        stack.push(newNode);
+        upHeap(newNode);
+    }
+
+
+    public void deleteItem(Object key) {
+        //removes the first node found that contains this key
+        Node nodeToDelete = locator.get(key);
+        if (nodeToDelete == null) throw new NoSuchElementException("Item to delete does not exist in heap");
+        else {
+            //upHeap(nodeToDelete);
+            swapNodes(lastItem, nodeToDelete);
+            Node pLast = stack.peek().parent;
+            if (pLast.right == null) {
+                pLast.left = null;
+            } else {
+                if (pLast.right.key == key) {
+                    pLast.right = null;
+                } else {
+                    pLast.left = null;
+                }
+            }
+            Node removedNode = stack.pop();
+            removedNode.key = null;
+            locator.remove(key);
+            downHeapAfterDeleteItem(nodeToDelete);
+        }
+    }
+
+    public Object removeMin() {
+        //return key in root
+        //copy key in lastItem into root
+        //set one of the children of parent of lastItem to null
+        //perform downHeap
+        //set parentOfNextOpenPosition to be either parent of lastItem or
+        //   or node that has replaced this parent during downHeap
+        Object min = root.key;
+        //pop out lastItem so that we get new lastItem
+        stack.pop();
+        printAsDiagram();
+        swapNodes(root, lastItem);
+        Node pLast = lastItem.parent;
+        if (pLast != null) {
+            if (pLast.right == null) {
+                pLast.left = null;
+            } else {
+                if (pLast.right.key == lastItem.key) {
+                    lastItem.key = null;
+                    pLast.right = null;
+                } else {
+                    pLast.left = null;
+                }
+            }
+
+            lastItem = stack.peek();
+            downHeap();
+            lastItem = stack.peek();
+            locator.remove(min);
+            System.out.println(stack);
+            System.out.println(queue);
+            System.out.println(locator);
+        } else root = null;
+        return min;
+    }
+
+    private void upHeap(Node n) {
+        //swap keys in nodes along upward path from n
+        //till key in n is no longer smaller than key in parent
+        lastItem = n;
+        Node currentNode = n;
+        while (currentNode.parent != null) {
+            key1 = (Comparable) currentNode.key;
+            key2 = (Comparable) currentNode.parent.key;
+            if ((key1.compareTo(key2)) < 0) {
+                swapNodes(currentNode, currentNode.parent);
+            }
+            currentNode = currentNode.parent;
+        }
+    }
+
+    private void swapNodes(Node currentNode, Node parent) {
+        Object parentKey = parent.key;
+        parent.key = currentNode.key;
+        currentNode.key = parentKey;
+        locator.put(currentNode.key, currentNode);
+        locator.put(parent.key, parent);
+    }
+
+    private void downHeapAfterDeleteItem(Node d) {
+        //starting from position where formerly last node was placed,
+        // swap keys along a downward path from position where formerly last node was placed
+        //until heap invariant property is obtained
+
+        Node currentNode = d;
+        while (currentNode.left != null || currentNode.right != null) {
+            if (currentNode.right != null) {
+                Node semiMin = null;
+                key1 = (Comparable) currentNode.left.key;
+                key2 = (Comparable) currentNode.right.key;
+                if (key1.compareTo(key2) < 0) semiMin = currentNode.left;
+                else semiMin = currentNode.right;
+                key1 = (Comparable) semiMin.key;
+                key2 = (Comparable) currentNode.key;
+                if (key1.compareTo(key2) < 0) {
+                    swapNodes(semiMin, currentNode);
+                    currentNode = semiMin;
+                } else break;
+            } else {
+                key1 = (Comparable) currentNode.left.key;
+                key2 = (Comparable) currentNode.key;
+                if (key1.compareTo(key2) < 0) {
+                    swapNodes(currentNode.left, currentNode);
+                    currentNode = currentNode.left;
+                } else break;
+            }
+        }
+
+    }
+
+    private void downHeap() {
+        //starting from root, swap keys along a downward path from root
+        //until value in root is no longer greater than key in child
+        Node currentNode = root;
+        while (currentNode.left != null || currentNode.right != null) {
+            if (currentNode.right != null) {
+                Node semiMin = null;
+                key1 = (Comparable) currentNode.left.key;
+                key2 = (Comparable) currentNode.right.key;
+                if (key1.compareTo(key2) < 0) semiMin = currentNode.left;
+                else semiMin = currentNode.right;
+                key1 = (Comparable) semiMin.key;
+                key2 = (Comparable) currentNode.key;
+                if (key1.compareTo(key2) < 0) {
+                    swapNodes(semiMin, currentNode);
+                    currentNode = semiMin;
+                } else break;
+            } else {
+                key1 = (Comparable) currentNode.left.key;
+                key2 = (Comparable) currentNode.key;
+                if (key1.compareTo(key2) < 0) {
+                    swapNodes(currentNode.left, currentNode);
+                    currentNode = currentNode.left;
+                } else break;
+            }
+        }
+
+    }
+
+    /**
+     * Prints to the console a visual representation of this
+     * tree, relying on the nested class BTreePrinter
+     */
+    public void printAsDiagram() {
+        BTreePrinter.printNode(root);
+    }
+
+    /**
+     * Specialized nested class to produce
+     * a visual image of this tree. This is
+     * third-party code. To access the functionality
+     * of this class, make a call to the static method
+     * printNode like this:  printNode(root)
+     */
+    static class BTreePrinter {
+        public static void printNode(Node root) {
+            int maxLevel = BTreePrinter.maxLevel(root);
+            printNodeInternal(
+                    Collections.singletonList(root), 1, maxLevel);
+        }
+
+        @SuppressWarnings({"rawtypes", "unchecked"})
+        private static void printNodeInternal(List nodes,
+                                              int level, int maxLevel) {
+            if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
+                return;
+            int floor = maxLevel - level;
+            int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+            BTreePrinter.printWhitespaces(firstSpaces);
+            List newNodes = new ArrayList();
+            for (Object node : nodes) {
+                if (node != null) {
+                    System.out.print(((Node) node).key);
+                    newNodes.add(((Node) node).left);
+                    newNodes.add(((Node) node).right);
+                } else {
+                    newNodes.add(null);
+                    newNodes.add(null);
+                    System.out.print(" ");
+                }
+
+                BTreePrinter.printWhitespaces(betweenSpaces);
+            }
+            System.out.println("");
+
+            for (int i = 1; i <= endgeLines; i++) {
+                for (int j = 0; j < nodes.size(); j++) {
+                    BTreePrinter.printWhitespaces(firstSpaces - i);
+                    if (nodes.get(j) == null) {
+                        BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                        continue;
+                    }
+
+                    if (((Node) nodes.get(j)).left != null)
+                        System.out.print("/");
+                    else
+                        BTreePrinter.printWhitespaces(1);
+
+                    BTreePrinter.printWhitespaces(i + i - 1);
+
+                    if (((Node) nodes.get(j)).right != null)
+                        System.out.print("\\");
+                    else
+                        BTreePrinter.printWhitespaces(1);
+
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+                }
+
+                System.out.println("");
+            }
+
+            printNodeInternal(newNodes, level + 1, maxLevel);
+        }
+
+        static void printWhitespaces(int count) {
+            for (int i = 0; i < count; i++)
+                System.out.print(" ");
+        }
+
+        static int maxLevel(Node node) {
+            if (node == null)
+                return 0;
+
+            return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+        }
+
+        @SuppressWarnings("rawtypes")
+        static boolean isAllElementsNull(List list) {
+            for (Object object : list) {
+                if (object != null)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    class Node {
+        Node left;
+        Node right;
+        Node parent;
+        Object key;
+
+        public Node(Object key, Node left, Node right, Node parent) {
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+            this.key = key;
+        }
+
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    '}';
+        }
+    }
+}
