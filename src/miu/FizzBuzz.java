96acf50cbb75da6cc726ce2f5dc53923fa393c36
samuelbwambale <simplesam63@gmail.com>
2021-01-16 08:14:29 -0600

    solve fizzbuzz funtional way

--- src/miu/FizzBuzz.java
+++ src/miu/FizzBuzz.java
@@ -9,9 +9,11 @@ For numbers which are multiples of both three and five print “FizzBuzz”
 *
 */
 
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
 	public static void main(String[] args) {
-		fizzBuzz(100);
+		fizzBuzz2(100);
 	}
 	
 	static void fizzBuzz(int n) {
@@ -26,5 +28,11 @@ public class FizzBuzz {
 			} else
 				System.out.print(i + " ");
 		}
+	}
+
+	public static void fizzBuzz2(int n) {
+		IntStream.rangeClosed(1,n)
+				.mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+				.forEach(System.out::println);
 	}
 }
